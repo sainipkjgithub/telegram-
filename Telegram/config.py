5a51461dac6bcad0943c4a05307bca914e068fcd
sainipkjgithub <160867713+sainipkjgithub@users.noreply.github.com>
2024-12-10 12:49:58 +0530

    Create config.py

---
+++ Telegram/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Telegram Bot configuration
+TELEGRAM_TOKEN = "8169493568:AAHgkbPTtukjYrrQDOk8zFrMN7jubQmuqV8"
+TELEGRAM_API = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
+
+# Google Sheets API Script URL
+APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyBUVP4SA8hO4zuI2PkORJaAGWILEpGY229mFYPU_NxgTraSvYnAxI8M4mC2vY2ZskjHg/exec"
+
+# Paxsenix API URL
+PAXSENIX_API_URL = "https://api.paxsenix.biz.id/ai/gpt4o"
